44b276c10bd5e2a86bc7210e4ae4ca0dd8404850
reneeb <info@perl-services.de>
2020-11-19 12:43:36 +0100

    add translation

---
+++ Kernel/Language/de_CustomerUserCheckAndDelete.pm
@@ -0,0 +1,32 @@
+# --
+# Copyright (C) 2020 Perl-Services, http://www.perl-services.de
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (AGPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# --
+
+package Kernel::Language::de_CustomerUserCheckAndDelete;
+
+use strict;
+use warnings;
+
+use utf8;
+
+sub Data {
+    my $Self = shift;
+
+    my $Lang = $Self->{Translation};
+
+    return if ref $Lang ne 'HASH';
+
+    $Lang->{"Customer Users - check and delete"} = "Kundenbenutzer prüfen und löschen";
+    $Lang->{"Check Customer Users"}              = "Kundenbenutzer prüfen";
+    $Lang->{"Delete Customer Users"}             = "Kundenbenutzer löschen";
+    $Lang->{"Customer Users Check"}              = "Kundenbenutzerprüfung";
+    $Lang->{"Check and delete customer users."}  = "Kundenbenutzer prüfen und löschen";
+
+    return 1;
+}
+
+1;
